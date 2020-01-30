d2be451a7c3851e8f35eeee0c3c9d55ac33d0045
unknown <jasongudna@hotmail.com>
2020-01-30 12:37:56 +0000

    first commit

---
+++ max_int.py
@@ -0,0 +1,6 @@
+north_int = int(input("Number of cars travelling north: "))
+south_int = int(input("Number of cars travelling south: "))
+east_int = int(input("Number of cars travelling east: "))
+west_int = int(input("Number of cars travelling west: "))
+
+
